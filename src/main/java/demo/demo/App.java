a9ca28ed5e04574f06f2ccd3821de62b6e3190af
sriharinayudu <sriharinayudut@xyz>
2020-10-01 11:55:54 +0530

    demo commit by srihari

---
+++ src/main/java/demo/demo/App.java
@@ -0,0 +1,14 @@
+package demo.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   public int demo1(int a,int b)
+   {
+	   int c=a+b;
+	   return c;
+   }
+}
---
+++ src/test/java/demo/demo/AppTest.java
@@ -0,0 +1,23 @@
+package demo.demo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class AppTest 
+    
+{
+	
+    public void testApp()
+    {
+    	
+
+    	App a=new App();
+   int j= 	a.demo1(10, 20);
+
+      //  assertTrue( true );
+   
+   
+   }
+    }
